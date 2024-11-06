96f8a056e64f22de36e3a8775bbac8687c1004a5
Beatriz Ayade <biaayade@hotmail.com>
2024-11-06 17:53:24 -0300

    Delete módulo_terceiros.py

--- módulo_terceiros.py
+++
@@ -1,6 +0,0 @@
-# importação e uso de módulo de terceiros
-import requests
-
-url = "https://www.example.com"
-response = requests.get(url)
-print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
