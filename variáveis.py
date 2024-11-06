b16351538825bb4e8daee003a33b771071530c9f
Beatriz Ayade <biaayade@hotmail.com>
2024-11-06 17:52:48 -0300

    Delete variáveis.py

--- variáveis.py
+++
@@ -1,4 +0,0 @@
-# declarar variável sem caractere especial, sem acentuação ou sublinhado, diferenciação entra maiúsculas e minúsculas
-nomeCompleto = "Beatriz Ayade"
-nome_completo = "Beatriz Ayade"
-idade = 25
