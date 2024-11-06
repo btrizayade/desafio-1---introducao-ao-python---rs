a4ce1c650b8da0cce578cc411f1e58d134e4101b
Beatriz Ayade <biaayade@hotmail.com>
2024-11-06 17:53:06 -0300

    Delete sintaxe.py

--- sintaxe.py
+++
@@ -1,9 +0,0 @@
-# cometário de uma linha -> #
-"""comentário de mais de uma linha -> """
-# quebra de linha -> \
-# blocos, identação
-idade = 25
-if idade < 18:
-    print("menor de idade")
-else:
-    print("maior de idade")
